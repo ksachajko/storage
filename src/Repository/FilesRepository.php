c0e5f25c29d17eccf223704d020a7cfbddd691e7
Krzysztof Sachajko <bmxkrsa@berlingskemedia.dk>
2018-12-07 14:03:58 +0100

    S-4 Replaced filesystem abstraction, added new adapters

--- src/Repository/FilesRepository.php
+++ src/Repository/FilesRepository.php
@@ -2,28 +2,51 @@
 
 namespace App\Repository;
 
-use App\Service\FilesystemResolver;
+use App\Service\FilesystemsResolver;
+use League\Flysystem\Exception;
+use League\Flysystem\Filesystem;
 
 class FilesRepository
 {
-    private $filesystem;
+    private $filesystems;
 
-    public function __construct(FilesystemResolver $filesystemResolver)
+    public function __construct(FilesystemsResolver $filesystemResolver)
     {
-        $this->filesystem = $filesystemResolver->getFilesystem();
+        $this->filesystems = $filesystemResolver->getFilesystems();
     }
 
     public function getAll(): array
     {
-        return $this->filesystem->keys();
+        return $this->find();
     }
 
-    public function find(string $query)
+    public function find(string $query = ''): array
     {
-        $keys = $this->filesystem->keys();
+        $results = [];
 
-        return array_filter($keys, function ($key) use ($query) {
-            return strpos($key, $query) !== false;
+        foreach ($this->filesystems as $filesystem) {
+            try {
+                $filesystemResults = $this->findInFilesystem($filesystem, $query);
+
+                $results = array_merge($results, $filesystemResults);
+            } catch (\Exception $e) {
+                # TODO log exception
+            }
+        }
+
+        return $results;
+    }
+
+    private function findInFilesystem(Filesystem $filesystem, $query): array
+    {
+        $contents = $filesystem->listContents();
+
+        if (!$contents) return [];
+
+        if (!$query) return $contents;
+
+        return array_filter($contents, function ($item) use ($query) {
+            return strpos($item['basename'], $query) !== false;
         });
     }
 }
